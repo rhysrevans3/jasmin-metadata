fe035f4582bfcdab8e3b8881bf057c5dd62c8861
Matt Pryor <matt.pryor@stfc.ac.uk>
2016-08-26 14:58:51 +0100

    Add initial migrations and version number

--- jasmin_metadata/__init__.py
+++ jasmin_metadata/__init__.py
@@ -7,5 +7,7 @@ This app provides generic functionality for attaching metadata to objects.
 __author__ = "Matt Pryor"
 __copyright__ = "Copyright 2015 UK Science and Technology Facilities Council"
 
+__version__ = "0.1"
+
 
 default_app_config = '{}.apps.AppConfig'.format(__name__)
